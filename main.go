f4d639db91c929e8c09b110dc45b5e2eb4ffd1df
fox-kazuki-matsumoto <kazuki.matsumoto@fox-hound.jp>
2019-01-26 15:21:29 +0900

    Update binary name

--- main.go
+++ main.go
@@ -10,10 +10,9 @@ import (
 
 func main() {
 	app := cli.NewApp()
-	app.Name = "qiitasearch"
+	app.Name = "cli-sample"
 	app.Usage = "search qiita articles"
 	app.Action = func(c *cli.Context) error {
-		//fmt.Println("Hello friend!")
 		api.RunQiitaSearch()
 		return nil
 	}
